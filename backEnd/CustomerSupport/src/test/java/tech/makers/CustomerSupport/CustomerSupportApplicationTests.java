6cd41d60fbe3004fcec54542dca97fc913fc3c09
Michael Su <mailmsuu@gmail.com>
2022-12-16 16:19:22 +0000

    Setup for CustomerSupport Backend

---
+++ backEnd/CustomerSupport/src/test/java/tech/makers/CustomerSupport/CustomerSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.makers.CustomerSupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
