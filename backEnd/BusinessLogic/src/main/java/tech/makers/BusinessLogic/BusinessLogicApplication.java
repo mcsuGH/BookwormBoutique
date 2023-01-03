ca59b07824ca70efa7b75a84072f16d733b0a422
Michael Su <mailmsuu@gmail.com>
2023-01-03 15:09:35 +0000

    re-tracking all files

---
+++ backEnd/BusinessLogic/src/main/java/tech/makers/BusinessLogic/BusinessLogicApplication.java
@@ -0,0 +1,13 @@
+package tech.makers.BusinessLogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessLogicApplication.class, args);
+	}
+
+}
