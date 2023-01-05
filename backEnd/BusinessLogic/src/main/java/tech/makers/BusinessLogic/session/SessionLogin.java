74f0f977ee258a361fafd06aa6462a4371d95475
Michael Su <mailmsuu@gmail.com>
2023-01-05 14:22:05 +0000

    Add SessionLogin for login form

--- backEnd/BusinessLogic/src/main/java/tech/makers/BusinessLogic/session/SessionLogin.java
+++ backEnd/BusinessLogic/src/main/java/tech/makers/BusinessLogic/session/SessionLogin.java
@@ -1,2 +1,12 @@
-package tech.makers.BusinessLogic.session;public class SessionLogin {
+package tech.makers.BusinessLogic.session;
+
+public class SessionLogin {
+  String username;
+
+  String password;
+
+  public SessionLogin(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
 }
