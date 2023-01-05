bfe4971d2c11af481286843cb955ee3f4e825d42
Michael Su <mailmsuu@gmail.com>
2023-01-05 14:34:29 +0000

    add tokenauthorizationfilter and websecurityfilter to make sure http requests are permitted when authorised

--- backEnd/BusinessLogic/src/main/java/tech/makers/BusinessLogic/WebSecurityConfig.java
+++ backEnd/BusinessLogic/src/main/java/tech/makers/BusinessLogic/WebSecurityConfig.java
@@ -1,2 +1,47 @@
-package tech.makers.BusinessLogic;public class WebSecurityConfig {
+package tech.makers.BusinessLogic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import tech.makers.BusinessLogic.user.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .csrf().disable()
+        .addFilterAfter(new TokenAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+        .authorizeHttpRequests()
+        .requestMatchers(HttpMethod.POST, "/api/admin/users").permitAll()
+        .requestMatchers(HttpMethod.POST, "/api/customer/users").permitAll()
+        .requestMatchers("/api/**").authenticated()
+        .anyRequest().permitAll();
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider(@Autowired UserDetailsServiceImpl userDetailsService) {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
 }
