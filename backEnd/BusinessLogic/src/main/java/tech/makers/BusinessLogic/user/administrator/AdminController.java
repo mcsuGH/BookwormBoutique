c2e8d59f2546ef58cd15530d0638edd5c69e74f0
Michael Su <mailmsuu@gmail.com>
2023-01-06 15:03:28 +0000

    change controller to use Dto rather than User class

--- backEnd/BusinessLogic/src/main/java/tech/makers/BusinessLogic/user/administrator/AdminController.java
+++ backEnd/BusinessLogic/src/main/java/tech/makers/BusinessLogic/user/administrator/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import tech.makers.BusinessLogic.session.Session;
 import tech.makers.BusinessLogic.session.SessionServiceImpl;
 import tech.makers.BusinessLogic.user.User;
+import tech.makers.BusinessLogic.user.UserDto;
 import tech.makers.BusinessLogic.user.UserRepository;
 
 @RestController
@@ -22,9 +23,9 @@ public class AdminController {
   private PasswordEncoder passwordEncoder;
 
   @PostMapping("/api/admin/users")
-  public Session createAdmin(@RequestBody User user) {
-    User newUser = new User(user.getUsername(), passwordEncoder.encode(user.getPassword()), true);
+  public Session createAdmin(@RequestBody UserDto userDto) {
+    User user = new User(userDto.getUsername(), passwordEncoder.encode(userDto.getPassword()), true);
     String token = sessionService.generateToken(user.getUsername());
-    return new Session(newUser, token);
+    return new Session(user, token);
   }
 }
